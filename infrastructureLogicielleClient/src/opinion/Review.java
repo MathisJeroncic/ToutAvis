1a4e8faa70df7c4f03cd48a8851d91af22c8a72d
Terstadi <152892085+Terstadi@users.noreply.github.com>
2024-05-17 11:29:47 +0200

    Review + test

---
+++ infrastructureLogicielleClient/src/opinion/Review.java
@@ -0,0 +1,24 @@
+package opinion;
+
+public class Review {
+	private String login;
+	private String title;
+	private float mark;
+	private String comment;
+
+	public String getLogin() {
+		return this.login;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public float getMark() {
+		return this.mark;
+	}
+	
+	public String getComment() {
+		return this.comment;
+	}
+}
--- infrastructureLogicielleClient/src/tests/ReviewItemBookTest.java
+++ infrastructureLogicielleClient/src/tests/ReviewItemBookTest.java
@@ -10,5 +10,92 @@ import exceptions.NotItemException;
 
 public class ReviewItemBookTest {
 	
+	private static int reviewItemBookTestBadEntryTest(ISocialNetwork sn, String login, 
+			String password, String title, float mark,
+			String comment, String testId, String errorMessage){
+		int nbBooks = sn.nbBooks();
+		
+		try {
+			sn.reviewItemFilm(login, password, title, mark, comment);
+			System.out.println("Err " + testId + " : " + errorMessage);
+			return 1;
+		}catch (BadEntryException e) {
+			if (sn.nbBooks() != nbBooks) {
+				System.out.println("Err " + testId + " : BadEntry was thrown but the number of review was changed");
+				return 1;
+			} else
+				return 0;
+		}catch (Exception e) {
+			System.out.println("Err " + testId + " : unexpected exception. " + e);
+			e.printStackTrace();
+			return 1;
+		}		
+	}
+	
+	private static int reviewItemBookOKTest(ISocialNetwork sn, String login, 
+			String password, String title, float mark,
+			String comment, String testId, String errorMessage) {
+		int nbBooks = sn.nbBooks();
+		try {
+			sn.reviewItemFilm(login, password, title, mark, comment);
+			if (sn.nbBooks() != nbBooks + 1) {
+				System.out.println("Err " + testId + " : the number of review was not incremented");
+				return 1;
+			} else
+				return 0;
+		} catch (Exception e) {
+			System.out.println("Err " + testId + " : unexpected exception " + e);
+			e.printStackTrace();
+			return 1;
+		}
+	}
+	
+	private static int reviewItemBookNotMemberTest(ISocialNetwork sn, String login, 
+			String password, String title, float mark,
+			String comment, String testId, String errorMessage) {
+		int nbBooks = sn.nbBooks();
+		try {
+			sn.reviewItemFilm(login, password, title, mark, comment);
+			System.out.println("Err " + testId + " : " + errorMessage);
+			return 1;
+		} catch (NotMemberException e) {
+			if (sn.nbBooks() != nbBooks) {
+				System.out.println(
+						"Err " + testId + " : NotMemberException was thrown, but the number of review"
+								+ ""
+								+ " was changed");
+				return 1;
+			} else
+				return 0;
+		} catch (Exception e) {
+			System.out.println("Err " + testId + " : unexpected exception. " + e);
+			e.printStackTrace();
+			return 1;
+		}
+	}
+	
+	private static int reviewItemBookNotItemTest(ISocialNetwork sn, String login, 
+			String password, String title, float mark,
+			String comment, String testId, String errorMessage) {
+		int nbBooks = sn.nbBooks();
+		try {
+			sn.reviewItemFilm(login, password, title, mark, comment);
+			System.out.println("Err " + testId + " : " + errorMessage);
+			return 1;
+		} catch (NotItemException e) {
+			if (sn.nbBooks() != nbBooks) {
+				System.out.println(
+						"Err " + testId + " : NotItemException was thrown, but the number of review"
+								+ ""
+								+ " was changed");
+				return 1;
+			} else
+				return 0;
+		} catch (Exception e) {
+			System.out.println("Err " + testId + " : unexpected exception. " + e);
+			e.printStackTrace();
+			return 1;
+		}
+	}
 	
 }
